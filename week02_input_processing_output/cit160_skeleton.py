a6789cf28e15e638b34b7c8b3717c36c47d6fe34
keen-coder <kknaur@gmail.com>
2026-01-21 11:52:04 -0500

    Updated Week 02 examples

--- week02_input_processing_output/cit160_skeleton.py
+++ week02_input_processing_output/cit160_skeleton.py
@@ -2,7 +2,7 @@
 # File Name:    THE_NAME_OF_THIS_MODULE
 # Author:       YOUR_NAME
 # Date:         DATE_OF_COMPLETION
-# Course:       CIT260-## (Replace ## with your section number)
+# Course:       CIT160-## (Replace ## with your section number)
 # Description:  WRITE A BRIEF (4-5 SENTENCES) DESCRIPTION OF WHAT THIS MODULE 
 #               DOES. THE DESCRIPTION SHOULD BE CLEAR AND SHOULD HELP SOMEONE
 #               WHO HAS NEVER SEEN YOUR CODE BEFORE UNDERSTAND WHAT THEY ARE ABOUT
@@ -15,11 +15,10 @@
 
 def main():
     # Your code goes here, replace this comment and the pass keyword with your code.
+    # Make sure you indent your code by one tab so it is under the main() function.
     pass
 
 
 
 if __name__ == '__main__':
     main()
-
-
