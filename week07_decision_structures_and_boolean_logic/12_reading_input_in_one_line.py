7c823a58f40ff0a9de34767e51ed445621812f04
keen-coder <kknaur@gmail.com>
2026-03-02 13:00:54 -0500

    Updated Week 07 example 12

--- week07_decision_structures_and_boolean_logic/12_reading_input_in_one_line.py
+++ week07_decision_structures_and_boolean_logic/12_reading_input_in_one_line.py
@@ -5,15 +5,25 @@ def main():
     # You just need to understand enough to be able to adapt this example for
     # any program where you want to read multiple values from the console in
     # one line.
+
+    # Example reading integers:
     num1, num2, num3 = map(int, input('Enter three values separated by spaces: ').split())
 
-    print(num1)
-    print(num2)
-    print(num3)
+    # Example reading floats:
+    num1, num2, num3, num4 = map(float, input('Enter four values separated by spaces: ').split())
 
-    # Below is the same work as above, just split into multiple lines.
+    # Example reading strings:
+    # NOTE: This only works if your strings are all one word, if you are reading
+    # strings with multiple words per string you can't use this method. It will
+    # be better to read one string per line (the way you know how).
+    str1, str2, str3, str5, str5 = input('Enter five words separated by spaces: ').split()
 
-
+    # NOTE: You can change the mapped function between int and float depending
+    # on what values you are reading. 
+    #
+    # Reading multiple words does not require the use of the map() function.
+    # If you want to read more or less values, just add or remove the correct
+    # number of variable names to the left of the assignment (=)
 
 if __name__ == '__main__':
     main()
